07ec57f016d7426ad63b0d24987f737d5148051f
Kidel <tano.design@gmail.com>
2016-10-17 21:58:34 +0200

    change of method and architecture to give users more control

--- README.md
+++ README.md
@@ -30,14 +30,14 @@ I suggest little steps of an interval of max 100 values, every time logging poss
  * Now to store the search results you need to type: 
  
  ```bash
- node bingSearch.js >> failed.txt
+ node bingSearch.js X >> failed_X.txt
  ```
- 
+ * Replace X with 0 if you want the first 50 results for each query in your file, 1 for the following 50 and so on (up to 2 if you want 150 results for each query total).
  * Logging to a file will help you keep track of all the queries that Bing fails or refuses to reply to. Using '>>' will append the new logs on the previous file, but if you want more control you may want to use multiple log files so that you can see if your API key has expired.
  * If your given interval contains more the 1500 codes I recommend splitting it into 15 or more smaller intervals and run  bingSearch.js 15 times with at least 2 Bing Accounts (free API accounts only have 5000 free transactions, and to get 150 results the software needs to do 3 of them for each record, since the API is limited to 50 max results for 1 query).
  * Once you've run bingSearch.js as many times as you want with different intervals you should have a file called 'output/results.txt' with all different results (if you did everything correctly with no repetitions) and failed.txt that you can use as input (config.js -> filename) to try and get what you missed. 
- * As mentioned above, **if you fail all the requests in the current interval it means that you've reached your API limit or your request/minute limit**, so simply pause the work and try another day or in a few hours. If you still fail all the requests try with another API key **from a different account**. 
- * failed.txt will have any query that has failed in bingSearch.js, meaning that you can rename it set it as input in config.js.
+ * As mentioned above, **if you fail all the requests in the current interval it means that you've reached your API limit or your request/minute limit**, so simply pause the work and try on the time that should appear in the logs (it's GMT). If you still fail all the requests try with another API key **from a different account**. 
+ * failed_X.txt will have any query that has failed in bingSearch.js with X value, meaning that you can set it as input in config.js and try those queries again at the end.
  * Finally run: 
  
  ```bash
@@ -49,11 +49,11 @@ I suggest little steps of an interval of max 100 values, every time logging poss
   node --max_old_space_size=4096 --optimize_for_size --max_executable_size=4096 --stack_size=4096 getPages.js  > logsGet.txt
  ```
  
- * NB: this may take a **very (very) long time** to end since you'll probably going to have around 2 million web pages to download. You're free to use only 
+ * NB: this may take a **very long time** to end since you'll probably going to have around 2 million web pages to download. You're free to use this if you want to check your progress on the terminal 
  
  ```bash
  node --max_old_space_size=4096 --optimize_for_size --max_executable_size=4096 --stack_size=4096 getPages.js 
  ```
  
-  (without the log file) if you want to check your progress on the terminal (don't be afraid if the first results are all errors, it's normal since the requests run all in parallel and errors are faster).
+ * Don't be afraid if the first results are all errors, it's normal since the requests run all in parallel and errors are faster.
  * You'll have all the files you need in the output folder and in htmlFiles folder (probably several thousands of html files and a csv file with your surname).
--- bingSearch.js
+++ bingSearch.js
@@ -1,74 +1,52 @@
 var config = require('./config');
 
-var fs = require('graceful-fs');
+var fs = require('fs');
 var Bing = require('node-bing-api')({ accKey: config.apiKey });
 var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(config.filename)
+    input: fs.createReadStream(config.filename)
 });
 
-var attempts = [0, 1, 2];
-var fail = [];
-var skip =[];
-var succ =[];
-
-function sleep(time, callback) {
-    var stop = new Date().getTime();
-    while(new Date().getTime() < stop + time) {
-        ;
-    }
-    callback();
-}
+var arg = process.argv.slice(2)[0] * 1;
+var subEnd = false;
 
 lineReader.on('line', function (line) {
     var code = line.split("\t")[0] * 1;
-    var query = line.split("\t")[1];
+    var query = line.split("\t")[1].replace(/(\r\n|\n|\r)/gm,"");
     if(code>=config.startingFrom && code<=config.endingTo) {
-        //console.log(code, query);
-        attempts.every(function(i) {
-            if(typeof skip[code] == 'undefined')
-                Bing.web(query, {
-                    top: 50,  // Number of results (max 50)
-                    skip: 50 * i,   // Skip first x results
-                    options: ['DisableLocationDetection', 'EnableHighlighting']
-                }, function (error, res, body) {
-                    if(!error) {
-                        if(typeof body != 'undefined') {
-                            var print = JSON.stringify(body.d.results, null, 4);
-                            //console.log(code + " query " + query + " has given " + body.d.results.length + " results");
-                            fs.appendFile('output/results.txt', print, function (err) {
-                                if (err) {
-                                    if(typeof fail[code] == 'undefined' && typeof succ[code] == 'undefined')
-                                        fail[code] = query;
-                                    //console.log(code + "\t" + query);
-                                    //throw new FsException("FS " + err);
-                                }
-                                else {
-                                    succ[code] = query;
-                                    if(body.d.results.length < 50)
-                                        skip[code] = query;
-                                }
-                            });
-                        }
-                        else {
-                            skip[code] = query;
-                            succ[code] = query;
-                            //throw new BingException("" + code + " query " + query + " has given no results");
-                        }
+        if(!subEnd)
+            Bing.web(query, {
+                top: 50,  // Number of results (max 50)
+                skip: 50 * arg,   // Skip first x results
+                options: ['DisableLocationDetection', 'EnableHighlighting']
+            }, function (error, res, body) {
+
+                if(typeof res != 'undefined' && res.statusCode == "503") {
+                    console.log(code + "\t" + query + "\t ERR:SUBLIMIT " + res.statusMessage);
+                    var subEnd = true;
+                    return;
+                }
+
+                if (!error) {
+                    if (typeof body != 'undefined') {
+                        var print = JSON.stringify(body.d.results, null, 4);
+                        //console.log(code + " query " + query + " has given " + body.d.results.length + " results");
+                        fs.appendFile('output/results.txt', print, function (err) {
+                            if (err) {
+                                console.log(code + "\t" + query + "\t FS error " + err);
+                            }
+                        });
                     }
                     else {
-                        if(typeof fail[code] == 'undefined' && typeof succ[code] == 'undefined')
-                            fail[code] = query;
-                        //throw new BingException("" +code + " query " + query + " has given error " + error);
+                        // 0 results
+                        return false;
                     }
-                });
-        });
+                }
+                else {
+                    // bing errors like timeout
+                    console.log(code + "\t" + query + "\t Bing API error " + error);
+                }
+            });
     }
-});
-
-lineReader.on('close', function() {
-    sleep(1000 * ((config.endingTo-config.startingFrom)/30), function() {
-        for(var code in fail) {
-            console.log(code + "\t" + fail[code]);
-        }
-    });
+}).on('close', function() {
+    console.log(fails);
 });
