f2462b27ec2bd9170dd70048949a814a3f802f50
Kidel <tano.design@gmail.com>
2016-10-18 01:30:57 +0200

    fixed a huge memory leak and made it way faster

--- getPages.js
+++ getPages.js
@@ -7,26 +7,38 @@ var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('output/' + config.cognome + '.csv')
 });
 
+function savePage(data, filename) {
+    fs.writeFile(filename, data, function (err) {
+        if (err) {
+            console.log("FS ERROR for file " + filename + ": " + e.message);
+        }
+        else console.log("OK FS file " + filename + "written correctly");
+    });
+}
+
 lineReader.on('line', function (line) {
-	var lineArr = line.split(";");
+    var lineArr = line.split(";");
     var query = lineArr[0];
     var filename = 'htmlFiles/' + lineArr[1];
-	var url = lineArr[2];
+    var url = lineArr[2];
     try {
-		var file = fs.createWriteStream(filename);
-		request.get({
-				url: url,
-				"rejectUnauthorized": false, 
-				headers: {
-					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
-				}
-		}).on('error', function (e) {
-			console.log("HTTP error getting query result for " + query + " url " + url + " ---- " + e.message);
-		}).pipe(file).on('close', function () {
-			console.log("Ok on file "+ filename +", query result for " + query + " url " + url);
-		});
-	}
-	catch(e) {
-		console.log("Got error for file " + filename + ": " + e.message);
-	}
+        var options = {
+            url: url,
+            method: 'GET',
+            "rejectUnauthorized": false,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+            }
+        };
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("OK HTTP got data for " + url);
+                savePage(body, filename);
+            }
+            else console.log("HTTP ERROR: " + ((typeof response != 'undefined')? response.statusCode:'?') + " --- " + error + " on url " + url);
+        })
+    }
+    catch(e) {
+        console.log("ERROR: " + e.message);
+    }
 });
