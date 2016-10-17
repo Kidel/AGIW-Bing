4d61a2633075aa0f023d49442f36503bda81fb67
Kidel <tano.design@gmail.com>
2016-10-17 19:38:13 +0200

    separated csv file creation and page get

---
+++ getPages.js
@@ -0,0 +1,26 @@
+var config = require('./config');
+
+var fs = require('graceful-fs');
+var request = require('request');
+
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('output/' + config.cognome + '.csv')
+});
+
+lineReader.on('line', function (line) {
+	var lineArr = line.split(";");
+    var query = lineArr[0];
+    var filename = 'output/' + lineArr[1];
+	var url = lineArr[2];
+    try {
+		var file = fs.createWriteStream(filename);
+		request.get(url).on('error', function (e) {
+			console.log("HTTP error getting query result for " + query + " url " + url + " ---- " + e.message);
+		}).pipe(file).on('close', function () {
+			console.log("Ok on file "+ filename +", query result for " + query + " url " + url);
+		});
+	}
+	catch(e) {
+		console.log("Got error for file " + filename + ": " + e.message);
+	}
+});
--- makeCsv.js
+++ makeCsv.js
@@ -20,14 +20,8 @@ fs.readFile('output/results.txt', 'utf8', function (err,data) {
         var url = d.Url;
         incremental++;
         try {
-            var file = fs.createWriteStream("output/" + config.matricola + "-" + incremental + ".html");
-            request.get(url).on('error', function (e) {
-                console.log("HTTP error getting code " + code + " url " + url + " ---- " + e.message);
-            }).pipe(file).on('close', function () {
-                console.log("Ok on code " + code + " url " + url);
-            });
-            //console.log({code: code, filenale: config.matricola + "-" + incremental + ".html", url: url})
-            writer.write({code: code, filenale: config.matricola + "-" + incremental + ".html", url: url});
+            //console.log({query: code, filename: config.matricola + "-" + incremental + ".html", url: url})
+            writer.write({query: code, filename: config.matricola + "-" + incremental + ".html", url: url});
         }
         catch(e) {
             console.log("Got error: " + e.message);
