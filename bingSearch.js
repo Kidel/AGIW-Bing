4e72a4eee1ae3b441b7a85962ce8a1faedf84e20
Lorenzo <lorenzoariemma@gmail.com>
2016-10-17 23:36:23 +0200

    Now working with recursion

--- bingSearch.js
+++ bingSearch.js
@@ -3,7 +3,7 @@ var config = require('./config');
 var fs = require('fs');
 
 var argMax = config.argMax;
-if(config.apiKey.length != (argMax+1)){
+if(config.apiKey.length >= (argMax+1)){
     getDataFromBing(config.apiKey,0);
 } else {
     console.log("Not enough api keys");
@@ -18,9 +18,11 @@ function getDataFromBing(apiKey, arg){
     });
 
     lineReader.on('line', function (line) {
-        var code = line.split("\t")[0] * 1;
-        var query = line.split("\t")[1].replace(/(\r\n|\n|\r)/gm,"");
+        var lineArray = line.split("\t");
+        var code = lineArray[0] * 1;
+        
         if(code>=config.startingFrom && code<=config.endingTo) {
+            var query = lineArray[1].replace(/(\r\n|\n|\r)/gm,"");
                 Bing.web(query, {
                     top: 50,  // Number of results (max 50)
                     skip: 50 * arg,   // Skip first x results
@@ -52,14 +54,12 @@ function getDataFromBing(apiKey, arg){
                     }
                 });
         } else {
-            if(code<=config.endingTo){
-              lineReader.close();  
+            if(code==(config.endingTo+1)){
+                if(arg<argMax){
+                    console.log("Token! Changing API key to " + arg);
+                    getDataFromBing(apiKey, ++arg); 
+                } 
             }
-        }
-    }).on('close', function(){
-        if(arg<argMax){
-            console.log("Token! Changing API key");
-           getDataFromBing(apiKey, arg++); 
         }
     });
 }
